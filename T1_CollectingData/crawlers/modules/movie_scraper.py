b6d3c6014f515afb4b1b3eaf6de3d61e9f03ed29
ZeroX <hoangthinhstkt@gmail.com>
2024-03-21 17:57:37 +0700

    Form Crawl Idea

---
+++ T1_CollectingData/crawlers/modules/movie_scraper.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 14:24:43 2024
+
+@author: ADMIN
+"""
+
---
+++ T1_CollectingData/crawlers/modules/settings.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 14:52:32 2024
+
+@author: ADMIN
+"""
+
